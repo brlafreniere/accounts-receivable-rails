9696d7b023101deee59cd20041266f4f741ca8a9
Blaine Lafreniere <brlafreniere@gmail.com>
2022-09-19 15:16:13 -0600

    Add button for currency conversion to Yen
    - Extend to_money helper to receive currency param

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,6 +1,15 @@
 module InvoicesHelper
-  def to_money(cents)
-    return "$0.00" if cents == 0
-    "$#{cents.to_s.insert(-3, ".")}"
+  def to_money(cents, currency=nil)
+    currency ||= 'USD'
+    case currency
+
+    when 'USD'
+      return "$0.00" if cents == 0
+      "$#{cents.to_s.insert(-3, ".")}"
+    when 'JPY'
+      return "0¥" if cents == 0
+      converted_yen_value = CurrencyConverter.convert(from: 'USD', to: currency, cents: cents)
+      "#{number_with_delimiter(converted_yen_value, delimiter: ',')}¥"
+    end
   end
 end
